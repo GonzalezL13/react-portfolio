41fc326711eefdc2c4632f93e70b27f77d6ac02b
GonzalezL13 <luisgonzalez1331@icloud.com>
2020-10-20 21:04:07 -0700

    files updated for about, contact, footer, header, nav, page, pagecontent

---
+++ src/components/Header/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Header(props) {
+
+    return (
+        <header className="flex-row space-between px-1">
+            <h1>Luis</h1>
+        </header>
+    );
+}
+
+export default Header;

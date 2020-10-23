05dbfcebf420d2cb59e8acc49b487e039eac3e22
GonzalezL13 <luisgonzalez1331@icloud.com>
2020-10-22 22:21:55 -0700

    Updates

---
+++ precache-manifest.c7cd22b3645a1481874d518f08c101e5.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0195abe71ebf8713ae33c52028fcd19",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "b81bc0e04dd916b9b3d3",
+    "url": "/react-portfolio/static/css/main.f18265a9.chunk.css"
+  },
+  {
+    "revision": "9881c57bc95ea97ba6a3",
+    "url": "/react-portfolio/static/js/2.660b8393.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/react-portfolio/static/js/2.660b8393.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b81bc0e04dd916b9b3d3",
+    "url": "/react-portfolio/static/js/main.7c12349e.chunk.js"
+  },
+  {
+    "revision": "a05a49d9351ba3961537",
+    "url": "/react-portfolio/static/js/runtime-main.bc59954b.js"
+  },
+  {
+    "revision": "8f2edaa204f53c0f0a3b11446196374a",
+    "url": "/react-portfolio/static/media/budget-tracker.8f2edaa2.jpg"
+  },
+  {
+    "revision": "b26fe8c52cbb99e9f250d8886080625d",
+    "url": "/react-portfolio/static/media/my-schtick-kick.b26fe8c5.jpg"
+  },
+  {
+    "revision": "938f8fe0fe2e5880b3ef528c5188a096",
+    "url": "/react-portfolio/static/media/run-buddy.938f8fe0.jpg"
+  },
+  {
+    "revision": "92df45b8c2d79ca4626c6c48e77c0bc3",
+    "url": "/react-portfolio/static/media/xpensifi.92df45b8.jpg"
+  }
+]);
